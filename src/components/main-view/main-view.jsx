c88572592b36b794f1dbfb72f18d59ff70fe8687
wentdavid <davidwent@me.com>
2023-01-03 18:49:51 +0100

    more specific types to the propTypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-
-
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
