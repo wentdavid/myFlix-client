d5f8c9d5b7953828067849bf23ad20a725e10806
wentdavid <davidwent@me.com>
2023-01-09 16:36:09 +0100

    Install bootstrap

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 
+
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
