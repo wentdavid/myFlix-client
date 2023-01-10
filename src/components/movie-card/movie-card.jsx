2c9699d47fbbafd55fef788a0c698c39831092b2
wentdavid <davidwent@me.com>
2023-01-10 16:19:18 +0100

    Routing BrowserRouter for movie-card view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
@@ -9,14 +10,16 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Description}</Card.Text>
-        <Button
-          variant="primary"
-          onClick={() => {
-            onMovieClick(movie);
-          }}
-        >
-          View Details
-        </Button>
+        <Link to={`/movies/${movie._id}`}>
+          <Button
+            variant="primary"
+            onClick={() => {
+              onMovieClick(movie);
+            }}
+          >
+            View Details
+          </Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -26,7 +29,8 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    ImagePath: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
